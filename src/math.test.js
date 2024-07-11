5112d8671fc9e978846a5aca493373ae160f5ed4
zapodask <joaopedroemerim@gmail.com>
2024-07-10 23:14:36 -0300

    init

---
+++ src/math.test.js
@@ -0,0 +1,17 @@
+const { add, subtract, multiply, divide } = require("./math");
+
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test("subtract", () => {
+  expect(subtract(3, 2)).toBe(1);
+});
+
+test("multiply", () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+test("divide", () => {
+  expect(divide(6, 3)).toBe(2);
+});
